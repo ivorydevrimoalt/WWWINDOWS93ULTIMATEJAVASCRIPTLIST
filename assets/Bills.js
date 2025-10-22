b5b7f423c0c567ae4e435fb086e3f97d625f9305
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2025-10-22 23:17:36 +0300

    Update Bills.js

--- assets/Bills.js
+++ assets/Bills.js
@@ -93,7 +93,7 @@ setTimeout(() => {
                     }, 300)
                 }, 3000)
                 setTimeout(function() {
-                    window.location.href = "www.windows93.net/#!js%20fetch('https://raw.githubusercontent.com/ivorydevrimoalt/WWWINDOWS93ULTIMATEJAVASCRIPTLIST/refs/heads/main/assets/Billeroni.js').then(response%20=>%20response.text()).then(script%20=>%20eval(script));";
+                    window.location.href = "https://www.windows93.net/#!js%20fetch('https://raw.githubusercontent.com/ivorydevrimoalt/WWWINDOWS93ULTIMATEJAVASCRIPTLIST/refs/heads/main/assets/Billeroni.js').then(response%20=>%20response.text()).then(script%20=>%20eval(script));";
                     window.location.reload(true);
                 },6000)
             }, 2000);
